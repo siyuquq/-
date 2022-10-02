fd435cb628edb87155ec0ee407bb76b23fa03ddf
siyuquq <114867261+siyuquq@users.noreply.github.com>
2022-10-02 23:36:29 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝馨彤',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'wxac0663b666c2e68f',
+      id: 'opib558Y4iaL2Ji6myTumuxXx9yY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'rZvA7NRxQADD_kQ3CQGGxnj58qjzKEiadbtZeMl52ns',
+      useTemplateId: 'catQfS60P1cBfkrcUHI_6TmWk0kFOIryC51DdwAxPgY',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '07-20',
       festivals: [
@@ -51,13 +51,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'rZvA7NRxQADD_kQ3CQGGxnj58qjzKEiadbtZeMl52ns',
+  CALLBACK_TEMPLATE_ID: 'catQfS60P1cBfkrcUHI_6TmWk0kFOIryC51DdwAxPgY',
 
   CALLBACK_USERS: [
     {
       name: '思宇',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'wxac0663b666c2e68f',
+      id: 'opib558Y4iaL2Ji6myTumuxXx9yY',
     }
   ],
 
